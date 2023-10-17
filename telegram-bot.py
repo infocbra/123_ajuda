9f717392336d0846d53d8b02df4dff4b5a97a838
LaisAxx <laisasss@hotmail.com>
2023-10-16 22:56:49 -0300

    porta 443 teste

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,7 +1,7 @@
 import requests
 
 TELEGRAM_API_TOKEN = "1856607725:AAFtHus3lxRHHMI2yZr6iVAPoxohOQwkE1s"
-WEBHOOK_URL = "https://123ajuda.tech:5005/webhook"
+WEBHOOK_URL = "https://123ajuda.tech:443/webhook"
 
 url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/setWebhook"
 payload = {
