8922c11ae04ee4db859800aa9d451be13b501746
LaisAxx <laisasss@hotmail.com>
2023-10-16 22:28:34 -0300

    alteracao url webhook

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,7 +1,7 @@
 import requests
 
 TELEGRAM_API_TOKEN = "1856607725:AAFtHus3lxRHHMI2yZr6iVAPoxohOQwkE1s"
-WEBHOOK_URL = "http://ec2-54-232-63-26.sa-east-1.compute.amazonaws.com/5005/webhook"
+WEBHOOK_URL = "https://123ajuda.tech:5005/webhook"
 
 url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/setWebhook"
 payload = {
