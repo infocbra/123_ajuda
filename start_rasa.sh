9ced2fa39f175169406a436c21c3e17c7b88ca69
LaisAxx <laisasss@hotmail.com>
2023-10-18 11:29:29 +0000

    alter em prod

--- start_rasa.sh
+++ start_rasa.sh
@@ -2,4 +2,5 @@
 
 source venv/bin/activate  # Ative o ambiente virtual do seu projeto Rasa, se você estiver usando um
 
-rasa run -m models --enable-api --endpoints endpoints.yml
+rasa run -m models --endpoints endpoints.yml --port 5005 --credentials credentials.yml --cors "*"
+ 
