d8dc0b89f59783c9e24cd08862e142485bab3da0
LaisAxx <laisasss@hotmail.com>
2023-10-18 09:33:56 -0300

    arquivo de log rasa

--- start_rasa.sh
+++ start_rasa.sh
@@ -2,6 +2,7 @@
 
 source venv/bin/activate  # Ative o ambiente virtual do seu projeto Rasa, se você estiver usando um
 
-rasa run -m models --endpoints endpoints.yml --port 5005 --credentials credentials.yml --cors "*"
+rasa run -m models --endpoints endpoints.yml --port 5005 --credentials credentials.yml --cors "*" --debug > rasa_logs.txt 2>&1
+
  
 
