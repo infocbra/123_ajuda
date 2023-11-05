32041cb7360976779bc22a21b4fbec89d527f9b2
LaisAxx <laisasss@hotmail.com>
2023-11-05 13:07:58 -0300

    correcoes acao estado pre classificado

--- actions/actions.py
+++ actions/actions.py
@@ -11,8 +11,6 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-
-
 class ActionPegarNome(Action):
     def name(self) -> Text:
         return "action_pegar_nome"
@@ -25,7 +23,7 @@ class ActionPegarNome(Action):
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
         if not slot_value:
-            dispatcher.utter_message(text="Por favor, qual seu nome.")
+            dispatcher.utter_message(text="Por favor, qual seu nome?")
             return {"nome": None}
         return {"nome": slot_value}
     
@@ -71,6 +69,15 @@ class AddressFormValidation(FormValidationAction):
         return {"bairro": slot_value}
 
 
+class SetEstadoDFAction(Action):
+    def name(self) -> str:
+        return "action_set_estado_df"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, ddomain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        return [SlotSet("estado", "DF")]
+
 class ActionFiltrarEndereco(Action):
     def name(self) -> Text:
         return "action_filtrar_endereco"
@@ -92,6 +99,7 @@ class ActionFiltrarEndereco(Action):
 
         # Faça a chamada à sua API passando as informações do endereço
         url = "https://123ajuda.tech/api/unidades_de_saude"
+        url_localhost = "http://127.0.0.1:5000/api/unidades_de_saude"
         params = {"uf": estado, "abragencia": cidade}
 
         try:
@@ -110,9 +118,13 @@ class ActionFiltrarEndereco(Action):
                         endereco = endereco_filtrado.get("endereco")
                         bairro = endereco_filtrado.get("bairro")
                         uf = endereco_filtrado.get("uf")
+                        abrangencia = endereco_filtrado.get("abrangencia")
+                        
+                        horario_atendimento = "Horário Integral" if is_full_time_value else "Horário Parcial"
+
 
                         # Envie a resposta para o usuário
-                        mensagem = f"Unidade de Saude: {nome}\nEndereco: {endereco}\nBairro: {bairro}\nEstado: {uf}"
+                        mensagem = f"Abrangencia: {abrangencia}\nHorário de Atendimento: {horario_atendimento}\nUnidade de Saude: {nome}\nEndereço: {endereco}\nBairro: {bairro}\nEstado: {uf}"
                         dispatcher.utter_message(text=mensagem)
                 else:
                     dispatcher.utter_message(text="Nenhum endereço encontrado.")
